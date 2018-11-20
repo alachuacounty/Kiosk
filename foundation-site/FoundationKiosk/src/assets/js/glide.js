d8447de5378e4024d41d5df19465650570269259
youriwims <ywims1@gmail.com>
2018-11-20 18:41:39 +0000

    glider.js carousel not working, testing script reference

---
+++ foundation-site/FoundationKiosk/src/assets/js/glide.js
@@ -0,0 +1,5 @@
+import Glide from '@glidejs/glide';
+import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm';
+
+new Glide('.glide').mount()
+new Glide('.glide').mount({ Controls, Breakpoints })
