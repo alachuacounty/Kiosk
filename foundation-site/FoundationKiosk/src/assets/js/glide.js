b8f4b391c6a571169fe62a80e7ddfbc29d0c9ede
Youri <ywims1@gmail.com>
2018-11-21 12:03:31 -0500

    added job card slider

--- foundation-site/FoundationKiosk/src/assets/js/glide.js
+++ foundation-site/FoundationKiosk/src/assets/js/glide.js
@@ -1,5 +1,20 @@
-import Glide from '@glidejs/glide';
-import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm';
+window.addEventListener('load', () => {
 
-new Glide('.glide').mount()
-new Glide('.glide').mount({ Controls, Breakpoints })
+    const glide = new Glide('.glide', {
+        type: "carousel",
+        touchAngle: 45,
+        startAt: 0,
+        perView: 5.5,
+        keyboard: true,
+        gap: 50,
+        autoplay: false,
+        peek: {
+            before: 100,
+            after: 50
+        },
+
+    })
+
+    glide.mount()
+
+})
